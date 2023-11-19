ce598111247a70d29d8ec915bb5fddfd020df03b
Karma <49839333+Yo-Karma@users.noreply.github.com>
2023-11-19 23:05:28 +0000

    fix(ai/downloader): fix yt downloader

--- BACKEND/AI/modules/downloader/main.py
+++ BACKEND/AI/modules/downloader/main.py
@@ -5,11 +5,12 @@ def download(url):
     print('Downloading video...')
     yt = YouTube(url)
     video = yt.streams.filter(file_extension='mp4').get_highest_resolution()
-    filename = format('%s.mp4' % uuid.uuid4())
+    filename = format('%s.mp4' % uuid.uuid4()).replace('-', '_')
+    print('Downloading video to %s...' % filename)
 
 
     try:
-        video.download(filename=filename)
+        video.download(filename=filename, output_path='tmp/')
         print('Video downloaded successfully.')
         return filename
     except:
