f46afa4078c72d869d51eebb5951fa965efb75f4
Hamz <hamzdevelopment@gmail.com>
2023-11-19 14:11:23 +0000

    add: custom register page functionality

--- FRONTEND/crumbai/app/api/auth/[...nextauth]/options.ts
+++ FRONTEND/crumbai/app/api/auth/[...nextauth]/options.ts
@@ -2,15 +2,19 @@ import type { NextAuthOptions } from 'next-auth'
 import GitHubProvider from 'next-auth/providers/github'
 import CredentialsProvider from 'next-auth/providers/credentials'
 import { prisma } from '@/lib/prisma'
+import { compare } from 'bcrypt'
 
 export const options: NextAuthOptions = {
+    session: {
+        strategy: 'jwt'
+    },
     providers: [
         GitHubProvider({
             clientId: process.env.GITHUB_ID as string,
             clientSecret: process.env.GITHUB_SECRET as string
         }),
         CredentialsProvider({
-            name: "CrumbAI",
+            name: "Sign In",
             credentials: {
                 email: {
                     label: "Email:",
@@ -27,14 +31,69 @@ export const options: NextAuthOptions = {
                 // Need to verify credentials in database
                 // https://next-auth.js.org/configuration/providers/credentials
 
-                const user = { id: "42", email: "hamizarif17@gmail.com", password: "nextauth"}
+                // const user = { id: "42", email: "hamizarif17@gmail.com", password: "nextauth"}
 
-                if (credentials?.email === user.email && credentials?.password === user.password) {
-                    return user
-                } else {
+                // if (credentials?.email === user.email && credentials?.password === user.password) {
+                //     return user
+                // } else {
+                //     return null
+                // }
+
+                if (!credentials?.email || !credentials.password) {
                     return null
+                }
+
+                const user = await prisma.user.findUnique({
+                    where: {
+                        email: credentials.email
+                    }
+                })
+
+                if (!user) {
+                    return null
+                }
+
+                const isPasswordValid = await compare(
+                    credentials.password,
+                    user.password
+                )
+        
+                if (!isPasswordValid) {
+                    return null
+                }
+        
+                return {
+                    id: user.id + '',
+                    email: user.email,
+                    name: user.name,
+                    randomKey: 'Hey cool'
                 }
             }
         })
     ],
+    callbacks: {
+        session: ({ session, token }) => {
+          console.log('Session Callback', { session, token })
+          return {
+            ...session,
+            user: {
+              ...session.user,
+              id: token.id,
+              randomKey: token.randomKey
+            }
+          }
+        },
+        jwt: ({ token, user }) => {
+          console.log('JWT Callback', { token, user })
+          if (user) {
+            const u = user as unknown as any
+            return {
+              ...token,
+              id: u.id,
+              randomKey: u.randomKey
+            }
+          }
+          return token
+        }
+      } 
 }
