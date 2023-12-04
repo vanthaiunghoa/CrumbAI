96eecbed0e218fd1dbf772362217aefe5e94d712
Hamz <hamzdevelopment@gmail.com>
2023-12-04 21:56:10 +0000

    add: logo added to top of sidebar

--- FRONTEND/crumbai/components/sidebar.tsx
+++ FRONTEND/crumbai/components/sidebar.tsx
@@ -1,9 +1,29 @@
 "use client";
 
+import Link from "next/link";
+import Image from "next/image";
+import { Montserrat } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const montserrat = Montserrat({ weight: "600", subsets: ["latin"]});
+
 const Sidebar = () => {
     return (
         <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
-            Hello Sidebar Component
+            <div className="px-3 py-2 flex-1">
+                <Link href="/dashboard" className="flex items-center pl-3 mb-14">
+                    <div className="relative w-12 h-12 mr-4">
+                        <Image
+                            fill
+                            alt="Logo"
+                            src="/logo.png"
+                        />
+                    </div>
+                    <h1 className={cn("pt-4 text-2xl font-bold", montserrat.className)}>
+                        CrumbAI
+                    </h1>
+                </Link>
+            </div>
         </div>
     )
 }
