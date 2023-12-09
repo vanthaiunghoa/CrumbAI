1f481bba5293950bcd57120a51dbfe29d3802f4c
Karma <49839333+Yo-Karma@users.noreply.github.com>
2023-12-09 15:02:07 +0000

    add(AI/editing): init cropping and face detection

--- BACKEND/AI/modules/editing/main.py
+++ BACKEND/AI/modules/editing/main.py
@@ -24,8 +24,14 @@ def cut_video(filename, start_time, end_time, duration, description, i):
         filename = filename.replace('.mp4', '')
     os.system(f"ffmpeg -i tmp/{filename}.mp4 -ss {start_time} -to {end_time} -crf 20 -c:v libx264 -b:v 0 -c:a copy tmp/{i}_{filename}.mp4 -hide_banner -loglevel error")
     print(f'Video: {i}_{filename} cut. Description: {description}. Duration of cut: {duration} seconds.')
-    # Save video name and description to file
-    
     print('Video cut successfully.')
     return
+
+def face_dectection_and_crop(filename):
+    print('Starting face detection...')
+    # Check if file exists in the tmp directory
+    if not os.path.exists(f'tmp/{filename}.mp4'):
+        print('File does not exist in tmp directory. Exiting...')
+        return
+
 
