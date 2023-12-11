ef4034b18606d5148aec6ffe4dcc06c764da6ce6
Karma <49839333+Yo-Karma@users.noreply.github.com>
2023-12-11 17:58:30 +0000

    add(AI/face_detection): init

--- BACKEND/AI/modules/face_detection/main.py
+++ BACKEND/AI/modules/face_detection/main.py
@@ -2,8 +2,14 @@ import os
 
 def face_dectection_and_crop(filename):
     print('Starting face detection...')
-    # Check if file exists in the tmp directory
-    if not os.path.exists(f'tmp/{filename}.mp4'):
-        print('File does not exist in tmp directory. Exiting...')
-        return
+    processed_file = 0
+
+    for i in range(0, 10):
+        if os.path.exists(f'tmp/{i}_{filename}'):
+            processed_file += 1
+            print(f'Processing file {i}_{filename}')
+
+        else:
+            print(f'File {i}_{filename} does not exist. Skipping.')
+            continue
 
