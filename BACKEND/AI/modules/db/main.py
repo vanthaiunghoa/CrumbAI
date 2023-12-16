7d2d5893d1318411edb876b6d33b777b3426c3c5
Karma <49839333+Yo-Karma@users.noreply.github.com>
2023-12-16 22:37:52 +0000

    add(AI/db): status methods

--- BACKEND/AI/modules/db/main.py
+++ BACKEND/AI/modules/db/main.py
@@ -12,3 +12,26 @@ def connect(host, user, password, database):
     except mysql.connector.Error as err:
         print(f"Error: {err}")
         return None
+
+
+def set_status(mydb, unique_id, user_id, status):
+    cursor = mydb.cursor()
+    query = f"UPDATE video_status SET status = '{status}' WHERE unique_id = '{unique_id}' AND user_id = '{user_id}'"
+    cursor.execute(query)
+    mydb.commit()
+    cursor.close()
+
+def create_new_video(mydb, unique_id, user_id, status):
+    cursor = mydb.cursor()
+    query = f"INSERT INTO video_status (unique_id, user_id, status) VALUES ('{unique_id}', '{user_id}', '{status}')"
+    cursor.execute(query)
+    mydb.commit()
+    cursor.close()
+
+def get_status(mydb, unique_id, user_id):
+    cursor = mydb.cursor()
+    query = f"SELECT status FROM video_status WHERE unique_id = '{unique_id}' AND user_id = '{user_id}'"
+    cursor.execute(query)
+    result = cursor.fetchone()
+    cursor.close()
+    return result[0]
