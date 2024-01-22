26f068ecf011be42dbd04153d116c3ab8c0d3f51
Hamz <hamzdevelopment@gmail.com>
2024-01-22 14:54:40 +0000

    add: Thumbnail Page Form

--- FRONTEND/crumbai/app/(dashboard)/(routes)/thumbnails/constants.ts
+++ FRONTEND/crumbai/app/(dashboard)/(routes)/thumbnails/constants.ts
@@ -2,6 +2,46 @@ import * as z from "zod";
 
 export const formSchema = z.object({
     prompt: z.string().min(1, {
-        message: "Prompt is required",
-    })
-})
+        message: "Iamge prompt is required",
+    }),
+    amount: z.string().min(1),
+    resolution: z.string().min(1),
+});
+
+export const amountOptions = [
+    {
+        value: "1",
+        label: "1 Photo",
+    },
+    {
+        value: "2",
+        label: "2 Photos",
+    },
+    {
+        value: "3",
+        label: "3 Photos",
+    },
+    {
+        value: "4",
+        label: "4 Photos",
+    },
+    {
+        value: "5",
+        label: "5 Photos",
+    }
+];
+
+export const resolutionOptions = [
+    {
+        value: "144x144",
+        label: "TikTok Thumbnail",
+    },
+    {
+        value: "1920x1080",
+        label: "Youtube Thumbnail",
+    },
+    {
+        value: "1080x1080",
+        label: "Instagram Reels Thumbnail",
+    }, 
+];
