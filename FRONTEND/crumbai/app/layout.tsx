b67748172fcb10629279bb4710e19137691fb1a4
Hamz <hamzdevelopment@gmail.com>
2024-01-22 13:46:45 +0000

    update: layout.tsx, removed ThemeProvider

--- FRONTEND/crumbai/app/layout.tsx
+++ FRONTEND/crumbai/app/layout.tsx
@@ -2,7 +2,6 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter as FontSans } from "next/font/google"
 import { cn } from "@/lib/utils"
-import { ThemeProvider } from "@/components/theme-provider"
 
 export const fontSans = FontSans({
   subsets: ["latin"],
@@ -24,12 +23,7 @@ export default function RootLayout({
       <body className={cn(
           "min-h-screen bg-background font-sans antialiased",
           fontSans.variable
-        )}>          <ThemeProvider
-        attribute="class"
-        defaultTheme="dark"
-        enableSystem
-        disableTransitionOnChange
-      >{children}</ThemeProvider></body>
+        )}>{children}</body>
     </html>
   )
 }
