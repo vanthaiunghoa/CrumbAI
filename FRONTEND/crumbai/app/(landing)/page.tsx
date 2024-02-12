3339f308aca5333ab03c5ab1bfb2e0488f14a46c
Karma <49839333+Yo-Karma@users.noreply.github.com>
2024-02-12 07:05:51 +0000

    add(ui/landing-page): customer stories

--- FRONTEND/crumbai/app/(landing)/page.tsx
+++ FRONTEND/crumbai/app/(landing)/page.tsx
@@ -88,7 +88,60 @@ const LandingPage = () => {
           </Link>
         </div>
       </div>
+      <div className="bg-[#232323] px-10 pb-20">
+        <h2 className="text-center text-4xl text-white font-extrabold mb-10 pt-10">Customer Stories</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+          <Card className="bg-[#1e1e1e] border-none text-white">
+            <CardHeader>
+              <CardTitle className="flex items-center justify-between">
+                <div>
+                  <p className="text-lg">Mike</p>
+                  <p className="text-zinc-400 text-sm">Podcast Host</p>
+                </div>
+                <Avatar>
+                  <AvatarImage src="https://cdn.discordapp.com/attachments/386246427325497356/1202249948817805323/image.png?ex=65ccc584&is=65ba5084&hm=d006ceda9df10a7291df8c03fd70316080e6afb67d9e8339b35cbcdbb4f30286&" alt="@shadcn" />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+              </CardTitle>
 
+              <CardContent className="pt-4 px-0">
+                Crumbai's AI effortlessly turns my long interviews into captivating shorts, highlighting the best moments. It's boosted my online presence remarkably!              </CardContent>
+            </CardHeader>
+          </Card>
+          <Card className="bg-[#1e1e1e] border-none text-white">
+            <CardHeader>
+              <CardTitle className="flex items-center justify-between">
+                <div>
+                  <p className="text-lg">Emma</p>
+                  <p className="text-zinc-400 text-sm">Youtube Tech Reviewer</p>
+                </div>
+                <Avatar>
+                  <AvatarImage src="https://cdn.discordapp.com/attachments/386246427325497356/1202248944370069565/image.png?ex=65ccc495&is=65ba4f95&hm=720dca083e780364390868a740bc481dbb8f8fc9916523cf6b2e53806d06a937&" alt="@shadcn" />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+              </CardTitle>
+              <CardContent className="pt-4 px-0">
+                Crumbai is a lifesaver for creating engaging shorts from my lengthy tech reviews, making my content more accessible and shareable.              </CardContent>
+            </CardHeader>
+          </Card>
+          <Card className="bg-[#1e1e1e] border-none text-white">
+            <CardHeader>
+              <CardTitle className="flex items-center justify-between">
+                <div>
+                  <p className="text-lg">Carlos</p>
+                  <p className="text-zinc-400 text-sm">TikTok Influencer</p>
+                </div>
+                <Avatar>
+                  <AvatarImage src="https://cdn.discordapp.com/attachments/386246427325497356/1202249289427062824/image.png?ex=65ccc4e7&is=65ba4fe7&hm=929248101114dea4c93743ace5e3d608391dc6ba3222a159858b12974d664814&" alt="@shadcn" />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+              </CardTitle>
+              <CardContent className="pt-4 px-0">
+                Crumbai transforms my daily vlogs into viral TikTok shorts. It intuitively picks the highlights, skyrocketing my engagement!              </CardContent>
+            </CardHeader>
+          </Card>
+        </div>
+      </div>
     </>
   )
 }
