527297ceffada5299c11ca78aa065d89d0a7cb0f
Karma <49839333+Yo-Karma@users.noreply.github.com>
2024-02-18 20:31:50 +0000

    add(ui/settings): plan status implementation

--- FRONTEND/crumbai/app/(dashboard)/(routes)/settings/page.tsx
+++ FRONTEND/crumbai/app/(dashboard)/(routes)/settings/page.tsx
@@ -1,7 +1,25 @@
+import { Heading } from '@/components/heading';
+import { SubscriptionButton } from '@/components/subscription-button';
+import { checkSubscription } from '@/lib/subscription';
+import { Settings } from "lucide-react";
+
 const SettingsPage = async () => {
+    const isUnlimited = await checkSubscription();
     return (
         <div>
-
+            <Heading
+                title="Settings"
+                description="Manage your account settings"
+                icon={Settings}
+                iconColor="#F3B13F"
+                bgColor="bg-violet=500/10"
+            />
+            <div className='px-4 lg:px-8 space-y-4'>
+                <div className="text-white text-sm">
+                    {isUnlimited ? "Current Plan: UNLIMITED." : "Current Plan: FREE TRIAL."}
+                </div>
+                <SubscriptionButton isUnlimited={isUnlimited} />
+            </div>
         </div>
     );
 }
---
+++ FRONTEND/crumbai/components/subscription-button.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import axios from "axios";
+import { useState } from "react";
+import { Zap } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+export const SubscriptionButton = ({
+    isUnlimited = false
+}: {
+    isUnlimited: boolean;
+}) => {
+  const [loading, setLoading] = useState(false);
+
+  const onClick = async () => {
+    try {
+      setLoading(true);
+
+      const response = await axios.get("/api/stripe");
+
+      window.location.href = response.data.url;
+    } catch (error) {
+      console.log("Something went wrong");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <Button variant={isUnlimited ? "default" : "crumbai"} disabled={loading} onClick={onClick} >
+      {isUnlimited ? "Manage Subscription" : "Upgrade"}
+      {!isUnlimited && <Zap className="w-4 h-4 ml-2" />}
+    </Button>
+  )
+};
