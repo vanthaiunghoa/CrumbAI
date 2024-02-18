f3516d2180a3a89eaf7c451d62e5583ca3b131e9
Hamiz Arif <62346333+HamzDevelopment@users.noreply.github.com>
2024-02-18 20:41:06 +0000

    update(frontend/audio): increase api limit on use

--- FRONTEND/crumbai/app/api/audio/route.ts
+++ FRONTEND/crumbai/app/api/audio/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import Replicate from "replicate";
+import { increaseApiLimit, checkApiLimit }  from "@/lib/api-limit";
 
 const replicate = new Replicate({
     auth: process.env.REPLICATE_API_TOKEN
@@ -14,6 +15,11 @@ export async function POST(req: Request) {
             return new NextResponse("Prompt are required", { status: 400 });
         }
 
+        const freeTrial = await checkApiLimit();
+        if (!freeTrial) {
+            return new NextResponse("You have exceeded the free trial limit", { status: 403 });
+        }
+
         const response = await replicate.run(
             "riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05",
             {
@@ -22,6 +28,8 @@ export async function POST(req: Request) {
             }
             }
         );
+
+        await increaseApiLimit();
 
         return NextResponse.json(response);
     } catch (error) {
