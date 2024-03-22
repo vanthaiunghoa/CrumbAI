1a0c097cbe0421bf2207419662707a3509a8d1d1
Karma <49839333+Yo-Karma@users.noreply.github.com>
2024-03-22 21:53:40 +0000

    refactor(AI/transcribe): method description

--- BACKEND/AI/modules/transcribe/main.py
+++ BACKEND/AI/modules/transcribe/main.py
@@ -2,17 +2,26 @@ from youtube_transcript_api import YouTubeTranscriptApi
 
 
 def transcribe(video_id):
+    """
+        Transcribe the video. (via YouTubeTranscriptApi)
+    """
     transcript = get_transcript(video_id)
     return format_transcript(transcript)
 
 
 def get_transcript(video_id):
+    """
+        Get the transcript of the video using YouTubeTranscriptApi.
+    """
     video_id = video_id.split('?v=')[1]
     transcript = YouTubeTranscriptApi.get_transcript(video_id)
     return transcript
 
 
 def format_transcript(formatted_transcript):
+    """
+        Format the transcript to srt format.
+    """
     formatted = ''
     for entry in formatted_transcript:
         start_time = "{:.2f}".format(entry['start'])
