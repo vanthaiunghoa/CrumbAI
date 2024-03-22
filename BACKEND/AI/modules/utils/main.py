cab17ff088a69f1a1e0c78451ce6087260d8791c
Karma <49839333+Yo-Karma@users.noreply.github.com>
2024-03-22 21:53:14 +0000

    add(AI/utils): move_dir method

--- BACKEND/AI/modules/utils/main.py
+++ BACKEND/AI/modules/utils/main.py
@@ -1,7 +1,28 @@
 import os
 import uuid
+
+
 def env(variable):
+    """
+        Get environment variable.
+    """
     return os.getenv(variable)
 
+
 def create_unique_id():
-    return format('%s.mp4' % uuid.uuid4()).replace('-', '_')
+    """
+        Create a unique id for the video.
+    """
+    return str(uuid.uuid4())
+
+
+def move_dir(filename, folder):
+    """
+        Move files to videos folder.
+    """
+    print(f'{folder}')
+    # create folder
+    if not os.path.exists(f'videos/{folder}'):
+        os.makedirs(f'videos/{folder}')
+    os.system(f'mv tmp/{filename} videos/{folder}/{filename}')
+    print('Files moved successfully.')
