330ec7802c5555327ee4bc0983145a80d47d6c30
Karma <49839333+Yo-Karma@users.noreply.github.com>
2024-04-04 02:59:47 +0100

    tweak(AI/main): move methods into other files, clean up and implementation of workers

--- BACKEND/AI/main.py
+++ BACKEND/AI/main.py
@@ -6,114 +6,72 @@ from modules.gpt.main import gpt
 from modules.downloader.main import download
 from modules.transcribe.main import transcribe
 from modules.editing.main import cut_video_up
-from modules.db.main import connect, set_status, create_new_video, get_status
+from modules.db.main import DB
 from modules.subtitles.main import create_srt
 from modules.subtitles.main import create_subtitles
 from modules.utils.main import env
-from modules.utils.main import create_unique_id
+from modules.utils.main import move_dir
 import json
 from rq import Worker, Queue
 from redis import Redis
 
-# listen = ['default']
-# conn = Redis(host="127.0.0.1", port=6379)
-# worker = Worker(map(Queue, listen), connection=conn)
-
 print('Starting Crumb AI...')
 load_dotenv()
-db = connect(env('DB_HOST'), env('DB_USER'), env('DB_PASSWORD'), env('DB_DATABASE'))
+db = DB(env('DB_HOST'), env('DB_USER'), env('DB_PASSWORD'), env('DB_DATABASE'))
 open_ai = gpt(env('OPENAI_API_KEY'), env('OPENAI_MODEL'))
 
+if __name__ == '__main__':
+    try:
+        worker = Worker(['default'], connection=Redis(host="161.97.88.202", port=6379, password="0fRhsy5lHQyDE6qC1mlB"))
+        worker.work()
+    except Exception as e:
+        print('Error connecting to Redis.')
+        print(e)
 
-def create(body):
-    print('Starting video creation...')
+def create(body, job_id):
     youtube_url = body['youtube_url']
     user_id = body['user_id']
     settings = body['settings']
-
-    if False:
-    # if does_it_exist(youtube_url):
-        print('Video already exists in database. Need to return already processed videos.')
-        videos = get_existing_data(youtube_url)
+    print(f'Creating video for {youtube_url} with settings {settings}.')
+    if db.does_it_exist(youtube_url):
+        videos = db.get_existing_data(youtube_url, settings)
     else:
-        print('Video does not exist in database. Need to process it.')
-        unique_id = create_unique_id()
-        create_new_video(db, unique_id, user_id, 'Video is being downloaded.')
-        print('Downloading video...')
-        filename = download(youtube_url, unique_id)
+        db.create_new_video(job_id, user_id, 'Video is being downloaded.', settings)
+        filename = download(youtube_url, job_id)
         if filename is None:
-            print('Error downloading video.')
+            db.set_status(user_id, user_id, 'Error downloading the video.')
             return
-        set_status(db, unique_id, user_id, 'Currently transcribing the video.')
+        db.set_status(job_id, user_id, 'Currently transcribing the video.')
         transcript = transcribe(youtube_url)
-        set_status(db, unique_id, user_id, 'Analyzing the content.')
+        db.set_status(job_id, user_id, 'Analyzing the content.')
         interesting_parts = open_ai.interesting_parts(transcript)
         content = json.loads(interesting_parts)
         formatted_content = content["segments"]
-        set_status(db, unique_id, user_id, 'Editing the video.')
+        db.set_status(job_id, user_id, 'Editing the video.')
         cut_video_up(filename, formatted_content)
 
-        save_to_database(youtube_url, filename, formatted_content)
+        print('Saving to database...')
+        db.save_to_database(youtube_url, job_id, formatted_content, user_id)
+        print('Saved to database.')
 
-        if settings['face_detection']:
-            set_status(db, unique_id, user_id, 'Cropping the video.')
+        if 'face_detection' in settings and settings['face_detection']:
+            db.set_status(job_id, user_id, 'Detecting faces within the video.')
             crop_video(filename)
 
-        if settings['subtitles']:
-            set_status(db, unique_id, user_id, 'Creating subtitles.')
+        if 'subtitles' in settings and settings['subtitles']:
+            db.set_status(job_id, user_id, 'Creating subtitles.')
             create_srt(filename)
             create_subtitles(filename)
 
-        set_status(db, unique_id, user_id, 'And we are done!')
+        if 'gameplay' in settings and settings['gameplay']:
+            db.set_status(job_id, user_id, 'Creating gameplay video.')
 
 
-def save_to_database(youtube_url, filename, formatted_content):
-    table = {}
-    for i in range(len(formatted_content)):
-        table[i] = {
-            "start_time": formatted_content[i]["start_time"],
-            "end_time": formatted_content[i]["end_time"],
-            "description": formatted_content[i]["description"],
-            "duration": formatted_content[i]["duration"],
-            "filename": f'{i}_{filename}'
-        }
+        db.set_status(job_id, user_id, 'Finalising the video.')
 
-    cursor = db.cursor()
-    query = "INSERT INTO videos (video_url, videos, user) VALUES (%s, %s, %s)"
-    values = (youtube_url, json.dumps(table), 'test')
-    cursor.execute(query, values)
-    db.commit()
-    return
+        for i in range(0, 10):
+            print(f'Checking if {i}_{filename} exists...')
+            if os.path.exists(f'tmp/{i}_{filename[:-4]}.mp4'):
+                move_dir(f'{i}_{filename}', filename[:-4])
 
-
-def get_existing_data(youtube_url):
-    ## todo: return all the data from the database, like how many videos have been processed, etc.
-    cursor = db.cursor()
-    query = f"SELECT * FROM videos WHERE video_url = '{youtube_url}'"
-    cursor.execute(query)
-    files = []
-
-    # Get files from drive
-    if query['videos'] is not None:
-        for video in query['videos']:
-            if not os.path.exists(f'tmp/{video.filename}'):
-                print('Video does not exist on disk. Need to process it.')
-                files.append(video.filename)
-
-    return files
-
-
-def does_it_exist(youtube_url):
-    cursor = db.cursor()
-    query = f"SELECT * FROM videos WHERE video_url = '{youtube_url}'"
-    cursor.execute(query)
-    result = cursor.fetchone()
-    if result is None:
-        return False
-    else:
-        return True
-
-
-if __name__ == '__main__':
-    #worker.work()
-    create({'youtube_url': 'https://www.youtube.com/watch?v=4Y4k0OPO5o0', 'user_id': 'test', 'settings': {'face_detection': True, 'subtitles': False}})
+        db.set_status(job_id, user_id, 'And we are done!')
